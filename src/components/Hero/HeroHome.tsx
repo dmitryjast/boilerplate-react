1756a49447d0173690aecce40a89141b2933f0b4
connor <dmitry.jatsyshyn@gmail.com>
2026-06-29 20:33:54 +0200

    Added part of the home slider component

--- src/components/Hero/HeroHome.tsx
+++ src/components/Hero/HeroHome.tsx
@@ -1,9 +1,50 @@
+import type { ReactNode } from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import { Autoplay, Pagination } from 'swiper/modules'
+
 import "./Hero.scss";
 
-export default function HeroHome() {
+interface SliderItem {
+    title?: string;
+    description?: string;
+    actions?: ReactNode;
+    background?: string;
+}
+
+interface SliderProps {
+    items: SliderItem[];
+    autoplay: boolean;
+    autoplayTiming: number;
+    pagination: boolean;
+}
+
+export default function HeroHome({items, autoplay, autoplayTiming, pagination}:SliderProps) {
     return(
-        <>
-        
-        </>
+        <Swiper
+            modules={[Autoplay, Pagination]}
+            autoplay={autoplay ? { delay: autoplayTiming } : false}
+            pagination={pagination ? { clickable: true } : false}
+        >
+            {items?.map((item, index) => (
+                <SwiperSlide key={index}>
+                    <div 
+                        className="slide-wrapper"
+                        style={item.background ? { backgroundImage: `url(${item.background})` } : undefined}
+                    >
+                        <div className="content-wrapper">
+                            {item.title && (
+                                <h1>{item.title}</h1>
+                            )}
+                            {item.description && (
+                                <p>{item.description}</p>
+                            )}
+                            {item.actions}
+                        </div>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
     )
 }
--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,8 +1,27 @@
+import HeroHome from "../../components/Hero/HeroHome";
+
 import "./Home.scss";
 
 export default function Home() {
     return(
         <>
+            <HeroHome 
+                items={[
+                    {
+                        title: "Slide 1",
+                    },
+                    {
+                        title: "Slide 2",
+                    },
+                    {
+                        title: "Slide 3",
+                    }
+                ]}
+            autoplay={false}
+            autoplayTiming={3000}
+            pagination={true}
+            
+            />
             <h1>Home</h1>
         </>
     )
